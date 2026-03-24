21d38cb8d68d325b147997be47c835c46a630a95
Fizz <113845465+fizzyob@users.noreply.github.com>
2026-03-24 20:03:39 +0800

    Update ChatGPT.js

--- classes/ChatGPT.js
+++ classes/ChatGPT.js
@@ -3,7 +3,8 @@ const BaseAI = require('./BaseAI');
 class ChatGPT extends BaseAI {
   constructor(requestModel, requestAuthorization, requestMessages) {
     super(requestModel, requestAuthorization, requestMessages);
-    this.url = 'https://api.openai.com/v1/chat/completions';
+    const openaiBaseUrl = (process.env.OPENAI_BASE_URL || 'https://api.openai.com/v1').replace(/\/$/, '');
+this.url = `${openaiBaseUrl}/chat/completions`;
   }
 }
 
